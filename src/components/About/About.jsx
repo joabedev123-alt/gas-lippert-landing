052252b8daa2dbdcdeb446bd7c7acfeda610ebbf
Your Name <you@example.com>
2026-08-31 12:57:36 -0300

    feat: update google maps embed to new address

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
           {/* Using a placeholder for the map. An iframe would be ideal here */}
           <div className={styles.mapPlaceholder}>
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111228.69466540608!2d-50.15582305!3d-29.7423275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95229db7ee2fbd5b%3A0xa645fb5b0e008ff7!2sCap%C3%A3o%20da%20Canoa%20-%20RS!5e0!3m2!1spt-BR!2sbr!4v1714578502395!5m2!1spt-BR!2sbr" 
+              src="https://maps.google.com/maps?q=Avenida+Paraguassu+3602,+Cap%C3%A3o+Novo,+Cap%C3%A3o+da+Canoa&t=&z=15&ie=UTF8&iwloc=&output=embed" 
               width="100%" 
               height="100%" 
               style={{ border: 0 }} 
